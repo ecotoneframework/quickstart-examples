40c71e81444c0684b97fc84ae1dbb525e8d690c3
Dariusz <dgafka.mail@gmail.com>
2024-02-19 07:47:32 +0000

    Symfony Multi-Tenancy support (#294)

--- MultiTenant/Symfony/EventSourcing/run_example.php
+++ MultiTenant/Symfony/EventSourcing/run_example.php
@@ -1,31 +1,31 @@
 <?php
 
 use App\MultiTenant\Application\Command\RegisterCustomer;
-use App\MultiTenant\Application\Command\RegisterProduct;
-use App\MultiTenant\Application\Command\UnregisterProduct;
-use Ecotone\EventSourcing\ProjectionManager;
+use App\MultiTenant\Configuration\Kernel;
 use Ecotone\Modelling\CommandBus;
 use Ecotone\Modelling\QueryBus;
-use Illuminate\Foundation\Http\Kernel;
-use Illuminate\Support\Facades\DB;
 use PHPUnit\Framework\Assert;
+use App\MultiTenant\Application\Command\RegisterProduct;
+use App\MultiTenant\Application\Command\UnregisterProduct;
 use Ramsey\Uuid\Uuid;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . "/../boostrap.php";
-$app = require __DIR__.'/bootstrap/app.php';
-$app->make(Kernel::class)->bootstrap();
-runMigrationForTenants(DB::connection('tenant_a_connection'), DB::connection('tenant_b_connection'));
+$kernel = new Kernel('dev', true);
+$kernel->boot();
+$app = $kernel->getContainer();
+runMigrationForTenants($kernel);
 
 /** @var CommandBus $commandBus */
 $commandBus = $app->get(CommandBus::class);
 /** @var QueryBus $queryBus */
 $queryBus = $app->get(QueryBus::class);
+
 echo "Running demo:\n";
 
 $laptopId = Uuid::uuid4();
 $commandBus->send(
-    new RegisterProduct($laptopId, "Laptop"),
+    new RegisterProduct($laptopId, 'Laptop'),
     metadata: [
         'tenant' => 'tenant_a'
     ]
